32833ccc8e5c96f71272474e88b79af005afe2bb
nana <nana@service-WIFI-EDUROAM-LA-31-253.nat.polymtl.ca>
2018-04-16 12:25:11 -0400

    test

---
+++ log1000/sources/Equipe.cpp
@@ -0,0 +1,8 @@
+//
+//  Equipe.cpp
+//  
+//
+//  Created by nana on 2018-04-16.
+//
+
+#include "Equipe.h"
---
+++ log1000/sources/Equipe.h
@@ -0,0 +1,23 @@
+#ifndef Equipe_H
+#define Equipe_H
+
+#include <string>
+
+using namespace std;
+
+class Equipe {
+    Equipe () {}
+    Equipe (string,string);
+}
+
+
+
+
+
+
+
+
+
+
+
+#endif
